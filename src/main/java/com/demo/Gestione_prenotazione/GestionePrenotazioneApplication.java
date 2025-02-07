9b931c60bb2d9e9224e59d27956b1f7414698d55
annunziatadr99 <anudar99@hotmail.it>
2025-02-07 16:30:01 +0100

    Progetto iniziato e impostato correttamente, ora procederò nella scrittura del codice

---
+++ src/main/java/com/demo/Gestione_prenotazione/GestionePrenotazioneApplication.java
@@ -0,0 +1,13 @@
+package com.demo.Gestione_prenotazione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionePrenotazioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionePrenotazioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/Gestione_prenotazione/GestionePrenotazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Gestione_prenotazione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePrenotazioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
